669b1c51d3d2e7ed83922cd808297e6c7bda7127
Ahmed Ansari <ahmedansari153@gmail.com>
2016-06-15 13:13:32 -0500

    Created database, dummy data, models, and project.

---
+++ src/Capstone/Migrations/20160615151610_MyFirstMigration.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Capstone.Migrations
+{
+    public partial class MyFirstMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Images",
+                columns: table => new
+                {
+                    ImageId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    ImageUrl = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Images", x => x.ImageId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Phones",
+                columns: table => new
+                {
+                    PhoneId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    AndroidVersion = table.Column<string>(nullable: true),
+                    BatteryMah = table.Column<int>(nullable: false),
+                    Camera = table.Column<string>(nullable: true),
+                    Carrier = table.Column<string>(nullable: true),
+                    DisplayResolution = table.Column<string>(nullable: true),
+                    ExpandableStorage = table.Column<bool>(nullable: false),
+                    GPU = table.Column<string>(nullable: true),
+                    ImageId = table.Column<int>(nullable: false),
+                    MicroUsb = table.Column<bool>(nullable: false),
+                    Price = table.Column<decimal>(nullable: false),
+                    Processor = table.Column<string>(nullable: true),
+                    QuickCharging = table.Column<bool>(nullable: false),
+                    RamSize = table.Column<int>(nullable: false),
+                    USBC = table.Column<bool>(nullable: false),
+                    WaterProof = table.Column<bool>(nullable: false),
+                    WirelessCharging = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Phones", x => x.PhoneId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Email = table.Column<string>(nullable: true),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.UserId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Images");
+
+            migrationBuilder.DropTable(
+                name: "Phones");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
---
+++ src/Capstone/Migrations/20160615152638_MyThirdMigration.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Capstone.Migrations
+{
+    public partial class MyThirdMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CPU",
+                table: "Phones");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CPU",
+                table: "Phones",
+                nullable: true);
+        }
+    }
+}
